456260ae945da0fb1419006b9c1427c02e8c6ea3
SukritD27 <sdhawan@sfsu.edu>
2023-07-28 23:59:36 -0700

    Implemented handlebars and completed milestone 2.

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Sukrit Dev Dhawan"});
 });
 
+router.get('/index.html', function(req, res, next) {
+  res.render('index', { title: 'CSC 317 App', name:"Sukrit Dev Dhawan"});
+});
+
+router.get('/login.html', function(req, res, next) {
+  res.render('login', { title: 'Login', name:"Sukrit Dev Dhawan"});
+  
+});
+
+router.get('/registration.html', function(req, res, next) {
+  res.render('registration', { title: 'Registration Form', name:"Sukrit Dev Dhawan"});
+  
+});
+
+router.get('/postvideo.html', function(req, res, next) {
+  res.render('postvideo', { title: 'Post Your Video', name:"Sukrit Dev Dhawan"});
+  
+});
+
+router.get('/viewvideo.html/:id(\\d+)', function(req, res, next) {
+  console.log(req.params);
+  res.render('viewvideo', { title: 'View Post', name:"Sukrit Dev Dhawan"});
+  
+});
+
 module.exports = router;
